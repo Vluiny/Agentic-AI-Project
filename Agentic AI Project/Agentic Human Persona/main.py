589a43f786c5bd474e4043faeccf444378bee2fb
Uzlvv <restupambudi2234@gmail.com>
2026-07-10 19:54:39 +0700

    Start Telegram bot with run_polling method
    Added application.run_polling() to start the bot.

--- Agentic AI Project/Agentic Human Persona/main.py
+++ Agentic AI Project/Agentic Human Persona/main.py
@@ -28,22 +28,16 @@ async def chat(update: Update, context: ContextTypes.DEFAULT_TYPE):
     full_messages = chat_history + [input_message]
 
     result = app.invoke({"messages": full_messages})
-    # reply = result["messages"][-1].content
+
     raw_reply = result["messages"][-1].content
 
-    # Jika Gemini mengembalikan string berupa list JSON bawaan
-
-    # 1. Pastikan bentuknya sudah jadi dictionary Python (bukan string)
     if isinstance(raw_reply, str):
         try: raw_reply = json.loads(raw_reply)
         except: pass
 
-    # 2. Langsung ambil teksnya
     try:
-        # Akses langsung: messages -> index 1 (AI) -> content -> index 0 -> text
         reply = raw_reply["messages"][1]["content"][0]["text"]
     except (KeyError, IndexError, TypeError):
-        # Fallback jika strukturnya tiba-tiba berbeda (misal cuma string biasa)
         reply = str(raw_reply)
 
         save_chat_history(user_id=chat_id, user_text=user_text, ai_reply=reply)
